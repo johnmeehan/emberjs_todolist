924f868ccfcef31fd61151c355c4dbd76b6558ad
John Meehan <john.meehan1@gmail.com>
2013-09-04 00:15:13 +0100

    Set up local storage

--- todo/js/controllers/todos_controller.js
+++ todo/js/controllers/todos_controller.js
@@ -19,6 +19,16 @@ Todos.TodosController = Ember.ArrayController.extend({
 		}
 	},
 
+	allAreDone: function(key, value){
+		if (value === undefined){
+			return !!this.get('length') && this.everyProperty('isCompleted', true);
+		}else {
+			this.setEach('isCompleted',value);
+			this.invoke('save');
+			return value;
+		}
+	}.property('@each.isCompleted'),
+
 	remaining: function(){
 		return this.filterProperty('isCompleted', false).get('length');
 	}.property('@each.isCompleted'),
